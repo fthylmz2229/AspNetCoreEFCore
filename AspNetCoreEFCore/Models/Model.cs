0ed4570d533e322e67c04ca3005a7de5e74d736d
fatih yilmaz <fthylmz2229@hotmail.com>
2019-04-29 21:27:37 +0300

    Add project files.

---
+++ AspNetCoreEFCore/Models/Model.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreEFCore.Models
+{
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext(DbContextOptions<BloggingContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Universite> Universiteler { get; set; }
+        public DbSet<Bolum> Bolumler { get; set; }
+    }
+
+    public class Universite
+    {
+        public int UniversiteId { get; set; }
+        public string UniversiteAdi { get; set; }
+        public List<Bolum> Bolumler { get; set; }
+    }
+
+    public class Bolum
+    {
+        public int BolumId { get; set; }
+        public string BolumAdi { get; set; }
+        public string Aciklama { get; set; }
+        public int UniversiteId { get; set; }
+        public Universite Universite { get; set; }
+    }
+}
